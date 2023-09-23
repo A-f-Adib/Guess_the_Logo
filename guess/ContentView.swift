8a1a8b8e29f203fc468b5bbaef2d1ed20af9d91e
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-09-24 04:14:10 +0600

    Initial Commit

---
+++ guess/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  guess
+//
+//  Created by A.f. Adib on 9/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ guess/guessApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessApp.swift
+//  guess
+//
+//  Created by A.f. Adib on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct guessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
